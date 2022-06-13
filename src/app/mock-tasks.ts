ff4dfe76f121274a3d6850ce885975bdee31c40a
Caro <grossocrln@gmail.com>
2022-06-13 13:34:20 -0300

    segundo commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar cursos de Jony",
+        day: "10 de Diciembre a las 10:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Preparar las cosas para el viaje",
+        day: "20 de Julio",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Pagar la matricula",
+        day: "3 de octubre",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Cumple mama",
+        day: "1 de julio",
+        reminder: false
+    }
+]
